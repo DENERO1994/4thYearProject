2e662aac95d8cb95288676414f26ec800be89f25
Robert Fitzgerald <robfitz16@gmail.com>
2018-01-18 12:42:38 +0000

    Initialised AWSmobileclient in the main activity

--- app/src/main/java/com/fourthyearproject/robsrecipes/MainActivity.java
+++ app/src/main/java/com/fourthyearproject/robsrecipes/MainActivity.java
@@ -3,11 +3,15 @@ package com.fourthyearproject.robsrecipes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(this).execute();
     }
 }
