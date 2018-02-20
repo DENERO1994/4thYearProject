579e8531329255da169a10c9bf6847f51009d98f
Robert Fitzgerald <robfitz16@gmail.com>
2018-02-20 11:38:09 +0000

    Add models and data folder for database files

---
+++ app/src/main/java/com/fourthyearproject/robsrecipes/data/UserDetailsDO.java
@@ -0,0 +1,56 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "robsrecipes-mobilehub-9460472-user-details")
+
+public class UserDetailsDO {
+    private String _userId;
+    private String _firstName;
+    private String _surname;
+    private String _userDetailsId;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBAttribute(attributeName = "firstName")
+    public String getFirstName() {
+        return _firstName;
+    }
+
+    public void setFirstName(final String _firstName) {
+        this._firstName = _firstName;
+    }
+    @DynamoDBAttribute(attributeName = "surname")
+    public String getSurname() {
+        return _surname;
+    }
+
+    public void setSurname(final String _surname) {
+        this._surname = _surname;
+    }
+    @DynamoDBAttribute(attributeName = "userDetailsId")
+    public String getUserDetailsId() {
+        return _userDetailsId;
+    }
+
+    public void setUserDetailsId(final String _userDetailsId) {
+        this._userDetailsId = _userDetailsId;
+    }
+
+}
